ca2acdf1291779817589eaec7394af158b1f10b8
Phillip Jon Ambas <ambasp@gmail.com>
2016-10-11 20:16:15 -0400

    [update] project name in view

--- api/resources/views/welcome.blade.php
+++ api/resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    Lareadux
+                    Laredux
                 </div>                
             </div>
         </div>
