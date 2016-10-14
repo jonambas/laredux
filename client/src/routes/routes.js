1eb5a238132880317a030ea1dd50e0d220029287
Phillip Jon Ambas <ambasp@gmail.com>
2016-10-14 11:49:40 -0400

    :sparkles: not found route

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -10,6 +10,7 @@ import Login from '../ui/components/auth/Login';
 import Register from '../ui/components/auth/Register';
 import Logout from '../ui/components/auth/Logout';
 import Dashboard from '../ui/components/Dashboard';
+import NotFound from '../ui/components/NotFound';
 
 const routes = (
   <Provider store={store}>
@@ -20,6 +21,7 @@ const routes = (
         <Route path="/login" component={Login} />
         <Route path="/logout" component={Logout} />
         <Route path="/dashboard" component={Dashboard} onEnter={verifyAuth} />
+        <Route path="*" component={NotFound} />
       </Route>
     </Router>
   </Provider>
---
+++ client/src/ui/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="flex center-xs middle-xs" style={{ height: '100%' }}>
+    <div className="col-xs-12" >
+      <h1>404</h1>
+    </div>
+  </div>
+);
+
+export default NotFound;
